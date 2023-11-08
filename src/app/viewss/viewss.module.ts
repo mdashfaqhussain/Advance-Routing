363f42b764e1f8173318282349d0e79ca08e7bb0
Mohammed Ashfaq <mdashfaqhussain510@gmail.com>
2023-11-08 17:13:50 +0530

    Basic Routing

---
+++ src/app/viewss/viewss.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ViewssRoutingModule } from './viewss-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ViewssRoutingModule
+  ]
+})
+export class ViewssModule { }
